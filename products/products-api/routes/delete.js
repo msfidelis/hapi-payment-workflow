90b80265cf1c85b9bbfb471d166ac1f6b2aeb34a
Matheus Fidelis <msfidelis@outlook.com>
2017-10-11 21:41:04 -0300

    Artigo 02

--- products/products-api/routes/delete.js
+++ products/products-api/routes/delete.js
@@ -2,16 +2,29 @@
 
 const ProductSchema = require('../models/Products');
 
-module.exports = [{
-    method: "DELETE",
-    path: "/products/{id}",
-    handler: (req, res) => {
-        ProductSchema.remove({ "_id": req.params.id })
-            .then(product => {
-                res(product).code(204);
-            })
-            .catch(err => {
-                console.log(err);
-            });
+const Joi = require('joi');
+const Boom = require('boom');
+
+module.exports = [
+    {
+        method: "DELETE",
+        path: "/products/{id}",
+        handler: (req, res) => {
+            ProductSchema.remove({ "_id": req.params.id })
+                .then(product => {
+                    res(product).code(204);
+                })
+                .catch(err => {
+                    console.log(err);
+                    res(Boom.internal(err));
+                });
+        },
+        config: {
+            validate: {
+                params: {
+                    id: Joi.string().required().min(20)
+                }
+            }
+        }
     }
-}]
+];
--- products/products-api/routes/update.js
+++ products/products-api/routes/update.js
@@ -2,16 +2,44 @@
 
 const ProductSchema = require('../models/Products');
 
-module.exports = [{
-    method: "PUT",
-    path: "/products/{id}",
-    handler: (req, res) => {
-        ProductSchema.findByIdAndUpdate(req.params.id, { $set: req.payload })
-            .then(result => {
-                res(result);
-            })
-            .catch(err => {
-                res(Boom.badRequest(err));
-            });
+const Joi = require('joi');
+const Boom = require('boom');
+
+module.exports = [
+
+    {
+        method: "PUT",
+        path: "/products/{id}",
+        handler: (req, res) => {
+            ProductSchema
+                .findByIdAndUpdate(req.params.id, {$set: req.payload}, {new: true})
+                .then(result => {
+
+                    if (!result) {
+                        let message = `Product ${req.params.id} not found`;
+                        res(Boom.notFound(message));
+                    } else {
+                        res(result);
+                    }
+
+                })
+                .catch(err => {
+                    res(Boom.internal(err));
+                });
+        },
+        config: {
+            validate: {
+                payload: {
+                    name: Joi.string(),
+                    description: Joi.string(),
+                    price: Joi.number(),
+                    tags: Joi.array()
+                }, 
+                query: {
+                    id: Joi.string()
+                }
+            }
+        }
     }
-}]
+
+]
